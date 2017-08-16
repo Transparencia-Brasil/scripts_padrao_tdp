37cd103029b517ac738881413014a0fa73eb0ede
voigtjessica <voigt.jessica@gmail.com>
2017-08-16 16:48:34 -0300

    Script padrão para upar planilha de contatos.

---
+++ contato_planilhas.R
@@ -0,0 +1,21 @@
+library(readxl)
+library(googlesheets)
+library(dplyr)
+
+url_contatos <- "https://docs.google.com/spreadsheets/d/1cxk1KUvncZ8SiavMmGkP4lYXjQ-7KD4corBjqkMjOxA/edit?usp=sharing"
+
+#Autenticação:
+gs_ls() 
+
+#Importando:
+contatos_sheet <- gs_title("TDP_Contatos_produção_nova")
+
+#Atribuindo o df a um objeto:
+contatos_tdp <- contatos_sheet %>%
+  gs_read()
+
+
+setwd("C:/Users/jvoig/Desktop/contatos/")
+write.table(contatos_tdp, file="contatos_upload.csv",
+            row.names = FALSE, fileEncoding = "utf-8",
+            quote = FALSE, sep=",", na= "")
