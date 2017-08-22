dbd4003d88b633436f5b31c0a65abb07e461fd19
voigtjessica <voigt.jessica@gmail.com>
2017-08-22 18:52:34 -0300

    script para consultas de obras que estão presentes no projeto Google. Sempre irá trabalhar com a planilha atualizada do SIMEC

---
+++ obras_consultas.R
@@ -0,0 +1,58 @@
+#Script padrão para verificação das obras do simec
+# A planilha é baixada do site do SIMEC
+
+library(dplyr)
+
+obras <- read.csv(url("http://simec.mec.gov.br/painelObras/download.php"), sep=";")
+
+#conferir que a importação foi bem feita:
+View(obras) 
+
+
+filtro_google_tipodeprojeto <- c("Espaço Educativo - 12 Salas",
+                                 "Espaço Educativo - 01 Sala",
+                                 "Espaço Educativo - 02 Salas",
+                                 "Espaço Educativo - 04 Salas",
+                                 "Espaço Educativo - 06 Salas",
+                                 "Projeto 1 Convencional",
+                                 "Projeto 2 Convencional")  
+
+
+filtro_google_situacao <- c("Inacabada",
+                            "Planejamento pelo proponente",
+                            "Execução",
+                            "Paralisada",
+                            "Contratação",
+                            "Licitação",
+                            "Em Reformulação")
+
+
+
+obras_google <- obras %>%
+  filter(Tipo.do.Projeto %in% filtro_google_tipodeprojeto ) %>%
+  filter(Situação %in% filtro_google_situacao ) 
+
+
+#Para ter uma lista de municípios com obras:
+#aqui não precisa alterar nada
+
+
+obras_google_munic <- obras_google %>%
+  mutate(num=1) %>%
+  group_by(Município, UF) %>%
+  summarise(obras = sum(num))
+
+write.table(obras_google_munic, file="obras_google_munic.csv", 
+            sep=";", row.names=F, na="")
+
+
+## Para conseguir as escolas em um município X 
+## (substituir o X pelo nome do município e o Y pelo estado)
+
+
+obras_google_X <- obras_google %>%                                      #substituir
+  filter(Município %in% c("X")) %>%                                     #substituir
+  select(id, Nome, Município, CEP, Logradouro, Bairro)
+
+write.table(obras_google_al, file="obras_google_al.csv", 
+            sep=";", row.names=F, na="", quote = F)
